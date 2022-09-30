39a6317ddbe1ecfb99383d5d631c5a94e2329118
Guleh <karel.verstreken@student.ehb.be>
2022-09-30 15:06:59 +0200

    added to admin

--- ghost/arq/base/admin.py
+++ ghost/arq/base/admin.py
@@ -7,8 +7,9 @@ class AssetAdmin(admin.ModelAdmin):
 class AlgorithmAdmin(admin.ModelAdmin):
     list_display = ('asset', 'name','last_updated')
 # Register your models here.
-from .models import Asset, Algorithm, Strategy, Tag
+from .models import Asset, Algorithm, Strategy, Tag, HitratioHistory
 admin.site.register(Asset, AssetAdmin)
 admin.site.register(Algorithm, AlgorithmAdmin)
 admin.site.register(Strategy)
 admin.site.register(Tag)
+admin.site.register(HitratioHistory)
