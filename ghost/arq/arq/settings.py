4bd7a18ba36f6b94a379dab768c672851ff815d9
EC2 Default User <ec2-user@ip-172-31-38-121.eu-west-3.compute.internal>
2022-09-30 12:27:10 +0000

    few changes

--- ghost/arq/arq/settings.py
+++ ghost/arq/arq/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get('DEBUG', 1)))
+DEBUG = False
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
