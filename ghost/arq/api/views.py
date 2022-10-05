766ca4433429c05e7b24cc93c3f6d117d6f0f1db
Guleh <karel.verstreken@student.ehb.be>
2022-10-05 16:22:18 +0200

    modified:   ghost/arq/api/views.py

--- ghost/arq/api/views.py
+++ ghost/arq/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from base.models import Asset, Algorithm
+from base.models import Asset, Algorithm, HitratioHistory
 from .serializers import AssetSerializer, AlgorithmSerializer
 
 @api_view(['GET'])
@@ -33,6 +33,7 @@ def getModels(request):
 def getModel(request, identifier):
     asset = Asset.objects.get(identifier=identifier)
     models = Algorithm.objects.filter(asset=asset)
+    models = HitratioHistory.objects.filter(asset=asset)
     serializer = AlgorithmSerializer(models, many=True)
     return Response(serializer.data)
 
